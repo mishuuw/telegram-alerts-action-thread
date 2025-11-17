413cd1fc7078dfd236982521200364b581b2015c
Михаил <katimerbo@gmail.com>
2025-11-17 14:36:57 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,27 @@ parse_mode=$5
 disable_web_page_preview=$6
 disable_notification=$7
 
-json="{\"chat_id\": \"$chat_id\", \"text\": \"$text\", \"message_thread_id\": $thread_id, \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": $disable_web_page_preview, \"disable_notification\": $disable_notification}"
+# Если текст содержит переносы строк, экранируем его через jq -Rs
+json=$(jq -n \
+  --arg chat_id "$chat_id" \
+  --arg thread_id "$thread_id" \
+  --arg text "$text" \
+  --arg parse_mode "$parse_mode" \
+  --argjson disable_web_page_preview "$disable_web_page_preview" \
+  --argjson disable_notification "$disable_notification" \
+  '{
+    chat_id: $chat_id,
+    message_thread_id: ($thread_id | tonumber?),
+    text: $text,
+    parse_mode: $parse_mode,
+    disable_web_page_preview: $disable_web_page_preview,
+    disable_notification: $disable_notification
+  }')
 
 echo "JSON payload:"
 echo "$json"
 
+# Отправка сообщения
 response=$(curl -s -X POST "https://api.telegram.org/bot$token_id/sendMessage" \
   -H "Content-Type: application/json" \
   -d "$json")
