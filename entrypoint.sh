e32d2d7ac1dbd9fed55b68ee6c491503469e10a5
Михаил <katimerbo@gmail.com>
2025-11-17 14:29:27 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,26 @@ parse_mode=$5
 disable_web_page_preview=$6
 disable_notification=$7
 
-response=`curl -X POST https://api.telegram.org/bot$token_id/sendMessage -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\": \"$text\", \"message_thread_id\": \"$thread_id\" \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}"}`
+payload=$(jq -Rn \
+  --arg chat_id "$chat_id" \
+  --arg thread_id "$thread_id" \
+  --arg text "$text" \
+  --arg parse_mode "$parse_mode" \
+  --arg disable_web_page_preview "$disable_web_page_preview" \
+  --arg disable_notification "$disable_notification" \
+  '{
+    chat_id: $chat_id,
+    text: $text,
+    message_thread_id: $thread_id,
+    parse_mode: $parse_mode,
+    disable_web_page_preview: ($disable_web_page_preview == "true"),
+    disable_notification: ($disable_notification == "true")
+  }'
+)
+
+response=$(curl -X POST \
+  -H "Content-Type: application/json" \
+  -d "$payload" \
+  "https://api.telegram.org/bot$token_id/sendMessage")
 
 echo "response=$response" >> $GITHUB_OUTPUT
