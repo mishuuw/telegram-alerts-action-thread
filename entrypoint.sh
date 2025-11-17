fd58a456a4969c5db8953b3329ec9dcf06639789
Михаил <katimerbo@gmail.com>
2025-11-17 14:17:30 +1000

    Refactor entrypoint.sh to include message_thread_id

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 token_id=$1
 chat_id=$2
-text=$3
-parse_mode=$4
-disable_web_page_preview=$5
-disable_notification=$6
+thread_id=$3
+text=$4
+parse_mode=$5
+disable_web_page_preview=$6
+disable_notification=$7
 
-response=`curl -X POST https://api.telegram.org/bot$token_id/sendMessage -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\": \"$text\", \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}"}`
+response=`curl -X POST https://api.telegram.org/bot$token_id/sendMessage -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\": \"$text\", \"message_thread_id\": \"$thread_id\" \"parse_mode\": \"$parse_mode\", \"disable_web_page_preview\": \"$disable_web_page_preview\", \"disable_notification\": \"$disable_notification\"}"}`
 
 echo "response=$response" >> $GITHUB_OUTPUT
